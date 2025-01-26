259ea1ed2da3c8524ee18f944f0fbcffde35fa3a
Aviansh7084 <169165516+Aviansh7084@users.noreply.github.com>
2025-01-26 10:37:05 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"Use strict" // treat all JS code  as newer version
+
+// alert(3 + 3) // we are using nodejs , not browser
+let score =33
+console.log(typeof score);
+
+console.log(3
+     + 
+     3);// code readability should be high
+
+console.log("avinash");
+
+let name= "avinash"
+let age = 18;
+let isLoggedIn= false
+
+
+// number -> 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null =< standalone value
+// undefined =>
+// symbol => unique 
+
+
+//Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
