9a129649f32ac0887824855fbcc4293afd399704
Aviansh7084 <169165516+Aviansh7084@users.noreply.github.com>
2025-01-26 10:37:50 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let accountEmail="chauhanavinash34@gmail.com"
+var accountPassword="12345"
+accountCity="Ballia"
+console.log(accountId);
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.log([accountId], accountEmail,accountPassword,accountCity);
+
+
+// accountId=2 not allowed
